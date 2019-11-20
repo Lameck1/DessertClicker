aa0db2781ed79658ceea068168914a226eea8742
Lameck Otieno <lameck72@gmail.com>
2019-11-20 13:08:54 +0300

    Initial commit

---
+++ app/src/main/java/com/lameck72/dessertclicker/ClickerApplication.kt
@@ -0,0 +1,11 @@
+package com.lameck72.dessertclicker
+
+import android.app.Application
+import timber.log.Timber
+
+class ClickerApplication: Application() {
+    override fun onCreate() {
+        super.onCreate()
+        Timber.plant(Timber.DebugTree())
+    }
+}
